7b0d8589b5336fd175ed9a09dbb32e6cc81b3298
pawel55 <pawel.tokarek@wp.pl>
2017-05-24 22:13:08 +0200

    My first ended game
    I used swings to do Frame.

---
+++ src/circleandcross/CircleandCross.java
@@ -0,0 +1,13 @@
+
+package circleandcross;
+
+
+public class CircleandCross {
+
+    public static void main(String[] args) {
+    GameFrame gameframe = new GameFrame() ;
+    gameframe.setVisible(true) ;
+    
+    }
+    
+}
